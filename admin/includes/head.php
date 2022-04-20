daf3c349924d3b753a958756b218549a91303799
IssacPacheco <isaacpacheco.go@gmail.com>
2022-04-20 16:55:09 -0500

    head de prueba

---
+++ admin/includes/head.php
@@ -0,0 +1,20 @@
+<title><?php echo $title;?></title>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, height=device-height">
+    <link rel="shortcut icon" href="../images/favicon.png"/>
+	<link href='https://fonts.googleapis.com/css?family=Open+Sans:400,600,700,300' rel='stylesheet' type='text/css'/>
+    
+    <!-- Styling -->
+    <link rel="stylesheet" href="../addons/bootstrap/css/bootstrap.css"/>
+    <link rel="stylesheet" href="../addons/toastr/toastr.min.css"/>
+    <link rel="stylesheet" href="../addons/fontawesome/css/font-awesome.css"/>
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.15.0/css/all.css">
+    <link rel="stylesheet" href="../addons/ionicons/css/ionicons.css"/>
+    <link rel="stylesheet" href="../addons/noUiSlider/nouislider.min.css"/>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/limonte-sweetalert2/6.11.0/sweetalert2.css"/>
+
+    <link rel="stylesheet" href="../styles/style.css"/>
+    <link rel="stylesheet" href="../styles/<?php echo $theme;?>" class="theme" />	
+    <!-- End of Styling -->
